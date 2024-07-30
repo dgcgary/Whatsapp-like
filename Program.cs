f38ddf091e6ea948887ccf8f195d2cdbbc10d10d
dgcgary <d.garcia.1@estudiantec.cr>
2024-07-29 21:53:59 -0600

    Metodo principal

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server {
+
+class Program {
+
+    // Método Principal
+    static void Main(string[] args)
+    {
+        if (args[0] == "-port"){
+            Servidor(Int32.Parse(args[1]));
+        }
+        else if (args[0] == "-msg"){
+            Cliente(Int32.Parse(args[1]), args[2]);
+        }
+        else {
+            Console.WriteLine("Uso\n Abrir server: -port <puerto>\nEnviar mensaje: -msg <puerto> <mensaje>");
+        }
+    }
+
+ 
