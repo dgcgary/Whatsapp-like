a20d54c59fcb43b535d6c401612832667220910c
dgcgary <d.garcia.1@estudiantec.cr>
2024-07-29 22:36:46 -0600

    Metodo cliente

--- Program.cs
+++ Program.cs
@@ -21,4 +21,39 @@ class Program {
         }
     }
 
- 
+    // Método Cliente: Establece una conexion TCP con el servidor, envia un mensaje y luego recibe una respuesta
+    static void Cliente(int port, string msg)
+    {
+        try {
+
+            IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddr, port);
+
+            Socket sender = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try {
+                sender.Connect(localEndPoint);
+                Console.WriteLine("| {0} |", sender.RemoteEndPoint.ToString());
+
+                byte[] messageSent = Encoding.ASCII.GetBytes(msg);
+                int byteSent = sender.Send(messageSent);
+
+                byte[] messageReceived = new byte[1024];
+                int byteRecv = sender.Receive(messageReceived);
+                Console.WriteLine("> {0}", Encoding.ASCII.GetString(messageReceived, 0, byteRecv));
+
+                sender.Shutdown(SocketShutdown.Both);
+                sender.Close();
+            }
+            catch (Exception e) {
+                Console.WriteLine("Excepción: {0}", e.ToString());
+            }
+        }
+        catch (Exception e) {
+            Console.WriteLine(e.ToString());
+        }
+    }
+
+}
+}
